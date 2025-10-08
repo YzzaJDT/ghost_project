a8a7e096ce584cf977784b4115d9fc4316e2f33f
YzzaJDT <124057340+YzzaJDT@users.noreply.github.com>
2025-10-08 22:37:22 +0800

    hayst

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,16 +12,13 @@ const MyFooter = () => {
     });
   }, []);
   return (
-   <Footer container>
-  <div className="w-full " data-aos="fade-up">
-    <Footer.Divider />
-    <div className="w-full sm:items-center text-gray-800">
+  
+    <div className="w-full sm:items-center text-gray-800 ">
       <p className="mb-1 text-center">
         © Copyright 2025 Rolling Hills Towing - All rights reserved
       </p>
     </div>
-  </div>
-</Footer>
+ 
   )
 }
 
