7db5e04d8dd47b176c474f2f2d8e081f290f01e9
YzzaJDT <124057340+YzzaJDT@users.noreply.github.com>
2025-10-08 20:53:42 +0800

    hmmm

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -65,12 +65,13 @@ try {
 
 return ( 
 <div className="py-12 w-full" id="feedback"> 
-  <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+  {/* <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
       We Value Your Feedback 
-    </h2>
-  <div className="w-full bg-white rounded-none md:rounded-2xl shadow-lg p-8 flex flex-col md:grid md:grid-cols-2 gap-8">
+    </h2> */}
+  {/* <div className="w-full bg-white rounded-none md:rounded-2xl shadow-lg p-8 flex flex-col md:grid md:grid-cols-2 gap-8"> */}
+    <div className="w-full bg-white rounded-none sm:rounded-md md:rounded-xl lg:rounded-2xl shadow-lg px-4 sm:px-8 lg:px-32 xl:px-64 py-10">
     {/* Left: QR + Review Section */}
-    <div className="w-full">
+    {/* <div className="w-full">
       <div className="grid md:grid-cols-2 mt-8">
         <div className="text-center">
           <img
@@ -95,11 +96,14 @@ return (
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
 
     {/* Right: Feedback Form */}
     <div className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-lg p-6 w-full">
       <form className="space-y-5 w-full" onSubmit={handleSubmit}>
+        <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+      We Value Your Feedback 
+    </h2>
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-1">
             Your Name
@@ -111,7 +115,7 @@ return (
             onChange={handleChange}
             placeholder="Enter your name"
             required
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            className="w-full border border-gray-300 rounded-lg py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
           />
         </div>
 
